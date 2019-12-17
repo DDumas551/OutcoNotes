2607a3b3a278f0fb091f3ba289955c8df02aeb2a
DDumas551 <david.j.dumas@gmail.com>
2019-12-16 17:20:45 -0800

    Wraps up last week's push

--- OutCo/TestCode.js
+++ OutCo/TestCode.js
@@ -1,36 +1,14 @@
-function evens(arr) {
-  if (arr.length === 0) return undefined;
-  let bucket = [];
+function findNumber(arr, k) {
+  if (arr.length < 1) return false;
+  if (arr.length > 10 ** 5) return false;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) bucket.push(arr[i]);
+    if (arr[i] > 10 ** 9) return false;
+    if (arr[i] !== k) {
+      console.log("NO");
+    } else {
+      return console.log("YES");
+    }
   }
-  return bucket;
 }
 
-// console.log(evens([4, 1, 5, 6, 7, 8, 10]));
-
-const odds = arr => {
-  if (arr.length === 0) return null;
-  let bucket = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 1) {
-      bucket.push(arr[i]);
-    }
-  }
-  return bucket;
-};
-
-// console.log(odds([1, 2, 3]));
-
-const threes = num => {
-  if (num <= 2 || num == 0) return null;
-  let bucket = [];
-  for (let i = 1; i < num; i++) {
-    if (i % 3 === 0) {
-      bucket.push(i);
-    }
-  }
-  return bucket;
-};
-
-console.log(threes(50));
+findNumber([1, 2, 3, 4, 5], 3);
